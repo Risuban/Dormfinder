794eaa963439900441afc6e140d934494fa323a2
Risuban <nicotapiamunoz@gmail.com>
2023-10-18 16:36:14 -0300

    added some views

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,25 @@ void main() async {
   runApp(const MyApp());
 }
 
+// Mock data: List of properties
+final List<Property> mockProperties = [
+  Property(
+    name: 'Casa Hermosa',
+    info1: '2 habitaciones, 1 baño',
+    info2: 'Cerca del parque',
+    price: 120000.0,
+    imageUrl: 'https://via.placeholder.com/150',
+  ),
+  Property(
+    name: 'Apartamento Centrico',
+    info1: '3 habitaciones, 2 baños',
+    info2: 'Vista a la ciudad',
+    price: 175000.0,
+    imageUrl: 'https://via.placeholder.com/150',
+  ),
+  // Add more mock properties as needed
+];
+
 class Property {
   final String name;
   final String info1;
@@ -29,7 +48,7 @@ class Property {
 class PropertyList extends StatelessWidget {
   final List<Property> properties;
 
-  const PropertyList({super.key, required this.properties});
+  const PropertyList({Key? key, required this.properties}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -37,7 +56,16 @@ class PropertyList extends StatelessWidget {
       itemCount: properties.length,
       itemBuilder: (context, index) {
         final property = properties[index];
-        return Card(
+        return GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => PropertyDetailsScreen(property: property),
+              ),
+            );
+          },
+          child: Card(
           margin: const EdgeInsets.all(8.0),
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(16.0),
@@ -70,7 +98,7 @@ class PropertyList extends StatelessWidget {
               ),
             ],
           ),
-        );
+        ));
       },
     );
   }
@@ -325,6 +353,186 @@ class ProfileScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Center(
       child: Text('Perfil'),
+    );
+  }
+}
+
+class PropertyDetailsScreen extends StatelessWidget {
+  final Property property;
+
+  PropertyDetailsScreen({required this.property});
+
+  @override
+  Widget build(BuildContext context) {
+    // Mock owner data
+    var owner = {
+      'phone': '123-456-7890',
+      'whatsapp': 'https://wa.me/1234567890', // this should be a real whatsapp link
+      'email': 'owner@example.com',
+      'name': 'John Doe'
+    };
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Detalles de propiedades',
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        backgroundColor: Colors.white,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Image.network(property.imageUrl), // display the image
+            Text(property.name),
+            Text(property.info1),
+            Text(property.info2),
+            Text('\$${property.price.toStringAsFixed(2)}'),
+            // Your other property details here...
+
+            // Property attributes table
+            DataTable(
+              columns: const [
+                DataColumn(label: Text('Attribute')),
+                DataColumn(label: Text('Value')),
+              ],
+              rows: [
+                DataRow(cells: [
+                  DataCell(Text('Location')),
+                  DataCell(Text('Google Maps Link')), // this should be a real link
+                ]),
+                // Other attributes here...
+              ],
+            ),
+
+            // Owner info table
+            DataTable(
+              columns: const [
+                DataColumn(label: Text('Contact')),
+                DataColumn(label: Text('Value')),
+              ],
+              rows: [
+                DataRow(cells: [
+                  DataCell(Text('Phone')),
+                  DataCell(Text(owner['phone']!)),
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('WhatsApp')),
+                  DataCell(Text(owner['whatsapp']!)),
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('Email')),
+                  DataCell(Text(owner['email']!)),
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('Name')),
+                  DataCell(Text(owner['name']!)),
+                ]),
+                // Other contact info here...
+              ],
+            ),
+            // Inside your PropertyDetailsScreen Widget
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => OwnerDetailsScreen(
+                      ownerName:'John Doe',  // replace with actual owner name
+                      ownerProperties: mockProperties,  // replace with actual list of owner's properties
+                      // pass other owner info here
+                    ),
+                  ),
+                );
+              },
+              child: Text('More Info'),
+            ),
+
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class OwnerDetailsScreen extends StatelessWidget {
+  final String ownerName;
+  final List<Property> ownerProperties;
+
+  // Add more parameters as needed for the owner's contact information
+  OwnerDetailsScreen({
+    required this.ownerName,
+    required this.ownerProperties,
+    // other owner info here
+  });
+
+    var owner = {
+      'phone': '123-456-7890',
+      'whatsapp': 'https://wa.me/1234567890', // this should be a real whatsapp link
+      'email': 'owner@example.com',
+      'name': 'John Doe'
+    };
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          '$ownerName',  // This displays the owner's name
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(color: Colors.black), // to change the back button color
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Contact Information',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+                        DataTable(
+              columns: const [
+                DataColumn(label: Text('Contact')),
+                DataColumn(label: Text('Value')),
+              ],
+              rows: [
+                DataRow(cells: [
+                  DataCell(Text('Phone')),
+                  DataCell(Text(owner['phone']!)),
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('WhatsApp')),
+                  DataCell(Text(owner['whatsapp']!)),
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('Email')),
+                  DataCell(Text(owner['email']!)),
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('Name')),
+                  DataCell(Text(owner['name']!)),
+                ]),
+                // Other contact info here...
+              ],
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Properties',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            Expanded(
+              child: PropertyList(properties: ownerProperties),  // Reusing the PropertyList widget
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
